c68d28c4809d57972a8bd86665dbc1d0a842b0c5
Alexandre Altgauzen <altgauzen@gmail.com>
2021-05-31 20:24:38 +0100

    requsito 10 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+/*Crie uma função que recebe um array de nomes de tecnologias que você quer aprender. Essa função deve receber também um segundo parâmetro chamado name com um nome.
+
+Para cada tecnologia no array, crie um objeto com a seguinte estrutura:
+
+  {
+  tech: "NomeTech",
+  name: name
 }
+Estes objetos devem ser inseridos em uma nova lista em ordem crescente a partir do campo tech no objeto.
+
+A saída da sua função deve ser uma lista de objetos ordenada pelo campo tech dos objetos com o formato acima.
+*/
+function techList(arrayTech, name) {
+  let techObject = [];
+    if (arrayTech.sort() === 0) {
+      return 'Vazio!';
+    } else {
+      for (let index = 0; index < arrayTech.length; index += 1)  {
+        techObject.push({tech:arrayTech[index], name:name});
+      }
+    }  
+  return techObject;
+}
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Alexandre'));
 
 // Desafio 11
 function generatePhoneNumber() {
